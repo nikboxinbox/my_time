5b9c252520fa7c92b3e2338d325c5ba997d3158c
nik_nik <nikbox88@gmail.com>
2023-11-12 15:33:50 +0300

    + logo

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,18 @@
 <template lang="pug">
-.ass ASSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSS  
+header 
+  a(href="#")
+    img.logo(src="./assets/logo.png", alt="Logo")
+  a(href="#")
+    div Day complete!
+main Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita vel quia adipisci id, molestias facilis eos at dignissimos, quam dolorum vero nihil. Consequuntur, dolore. In fugiat sequi labore quae commodi! Lorem ipsum dolor sit amet consectetur adipisicing elit. At ex fugit consectetur veritatis id totam excepturi corrupti earum minima! Laudantium aspernatur omnis minima distinctio ipsam sapiente illum doloremque impedit quisquam. Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quas, aut aliquam consequuntur rerum officia aperiam molestiae magnam, nam ut unde fuga repudiandae quam cupiditate? Et soluta neque autem deserunt. Nemo? Lorem ipsum, dolor sit amet consectetur adipisicing elit. Magni rerum dolore magnam similique doloremque fugiat repudiandae reprehenderit saepe delectus, temporibus quisquam eaque corporis, officia ad dolor nihil omnis quis! Hic? Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus fuga vitae consectetur veritatis adipisci nisi officiis, veniam beatae ut recusandae nemo maxime, ex, doloribus ad deleniti. Sed minus aliquid facere. Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque officia corrupti asperiores id nemo veritatis ut facilis dolorem et delectus consequatur voluptate incidunt possimus sapiente corporis modi, veniam aliquid! Quia! Lorem ipsum, dolor sit amet consectetur adipisicing elit. Facilis harum quibusdam atque accusantium! Nostrum praesentium rerum saepe alias tenetur asperiores quibusdam autem dignissimos expedita architecto, repellat nihil numquam. Quos, accusantium.
 </template>
-<style lang="scss">
-.ass {
-  width: 200px;
-  border: 1px solid $primary;
-  background-color: $primary;
+<style lang="scss" scoped>
+header {
+  position: sticky;
+  top: 0;
+  z-index: 20;
+}
+.logo {
+  height: 2.5rem;
 }
 </style>

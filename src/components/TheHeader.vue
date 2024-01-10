e06e301dccf2277a289db1a5b3c3a5246bc09f74
nik_nik <nikbox88@gmail.com>
2024-01-10 18:41:13 +0300

    + TheHeaderProgress

--- src/components/TheHeader.vue
+++ src/components/TheHeader.vue
@@ -1,18 +1,17 @@
 <script setup lang="ts">
 import { CheckCircleIcon } from '@heroicons/vue/24/solid'
 import TheLogo from './TheLogo.vue'
+import TheHeaderProgress from './TheHeaderProgress.vue'
 </script>
+
 <template lang="pug">
+
 header.header().shadow--bottom 
   TheLogo
-  .section-right--complete(v-if="true")
-    div Day complete!
-    CheckCircleIcon.icon__check-circle
-  .section-right--progress(v-else) 
-    div Progress: 
-    span 20%
-    .round
+  TheHeaderProgress
+
 </template>
+
 <style scoped lang="scss">
 .header {
   position: sticky;
@@ -23,40 +22,5 @@ header.header().shadow--bottom
   align-items: center;
   padding: 0.75rem;
   background-color: $white;
-
-  .section-right--complete {
-    display: flex;
-    align-items: center;
-    gap: 0.25rem;
-    div {
-      font-size: 1rem;
-      line-height: 1.25rem;
-    }
-    .icon__check-circle {
-      height: 1.75rem;
-      color: #10b981;
-    }
-  }
-
-  .section-right--progress {
-    display: flex;
-    align-items: center;
-    gap: 0.25rem;
-
-    div {
-      span {
-        display: flex;
-        align-items: center;
-        gap: 0.25rem;
-      }
-    }
-
-    .round {
-      height: 0.75rem;
-      width: 0.75rem;
-      border-radius: 9999px;
-      background-color: #ef4444;
-    }
-  }
 }
 </style>
---
+++ src/components/TheHeaderProgress.vue
@@ -0,0 +1,51 @@
+<script setup lang="ts">
+import { CheckCircleIcon } from '@heroicons/vue/24/solid'
+</script>
+
+<template lang="pug">
+
+.complete(v-if="true")
+  div Day complete!
+  CheckCircleIcon.icon__check-circle
+.progress(v-else) 
+  div Progress: 
+  span 20%
+  .round
+
+</template>
+<style scoped lang="scss">
+.complete {
+  display: flex;
+  align-items: center;
+  gap: 0.25rem;
+  div {
+    font-size: 1rem;
+    line-height: 1.25rem;
+  }
+  .icon__check-circle {
+    height: 1.75rem;
+    color: #10b981;
+  }
+}
+
+.progress {
+  display: flex;
+  align-items: center;
+  gap: 0.25rem;
+
+  div {
+    span {
+      display: flex;
+      align-items: center;
+      gap: 0.25rem;
+    }
+  }
+
+  .round {
+    height: 0.75rem;
+    width: 0.75rem;
+    border-radius: 9999px;
+    background-color: #ef4444;
+  }
+}
+</style>

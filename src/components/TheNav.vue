ecd53b46e48d840e5dd03d2048d3b262e3a196b6
nik_nik <nikbox88@gmail.com>
2024-03-24 14:23:17 +0300

    + constans.js for nav pages

--- src/components/TheNav.vue
+++ src/components/TheNav.vue
@@ -1,10 +1,27 @@
 <script setup lang="ts">
 import { ref } from 'vue';
 import { ClockIcon, ListBulletIcon, ChartBarIcon } from '@heroicons/vue/24/outline'
+import { PAGE_TIMELINE, PAGE_ACTIVITIES, PAGE_PROGRESS } from '../constants'
 import NavItem from './NavItem.vue';
 
-const navItems = { timeline: ClockIcon, activities: ListBulletIcon, progress: ChartBarIcon }
-const currentPage = ref('timeline');
+const navItems = {
+  [PAGE_TIMELINE]: ClockIcon,
+  [PAGE_ACTIVITIES]: ListBulletIcon,
+  [PAGE_PROGRESS]: ChartBarIcon
+}
+const currentPage = ref(normalizePageHash());
+
+
+function normalizePageHash() {
+  const hash = window.location.hash.slice(1);
+
+  if (Object.keys(navItems).includes(hash)) {
+    return hash
+  }
+  window.location.hash = PAGE_TIMELINE
+
+  return PAGE_TIMELINE
+}
 
 </script>
 

32ba019b552ac2156801a3ddf2e6d4678405d580
nik_nik <nikbox88@gmail.com>
2024-04-20 12:08:03 +0300

    settings linter

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,12 +1,31 @@
-/* eslint-env node */
+// /* eslint-env node */
+// require('@rushstack/eslint-patch/modern-module-resolution')
+
+// module.exports = {
+//   root: true,
+//   extends: [
+//     'plugin:vue/vue3-essential',
+//     'eslint:recommended',
+//     '@vue/eslint-config-typescript',
+//     '@vue/eslint-config-prettier/skip-formatting'
+//   ],
+//   parserOptions: {
+//     ecmaVersion: 'latest'
+//   },
+//   rules: {
+//     // rules configuration here <-----
+
+//     'no-unused-vars': 'off'
+//     // '@typescript-eslint/no-unused-vars': 'error'
+//   }
+// }
 require('@rushstack/eslint-patch/modern-module-resolution')
 
 module.exports = {
   root: true,
-  'extends': [
+  extends: [
     'plugin:vue/vue3-essential',
     'eslint:recommended',
-    '@vue/eslint-config-typescript',
     '@vue/eslint-config-prettier/skip-formatting'
   ],
   parserOptions: {

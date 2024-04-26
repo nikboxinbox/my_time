a3cf0a60017734903559fd863c9282663a92daaf
nik_nik <nikbox88@gmail.com>
2024-04-26 23:26:39 +0300

    + TockenService > generateTokens & saveToken methods

--- server/service/tocken-service.js
+++ server/service/tocken-service.js
@@ -1,3 +1,30 @@
-class TockenService {}
+import { sign } from 'crypto'
+import jwt from 'jsonwebtoken'
+import tokenModel from '../models/token-model'
+
+class TockenService {
+  generateTokens(payload) {
+    const accessToken = jwt.sign(payload, process.env.JWT_ACCESS_SECRET, { expiresIn: '30m' })
+    const refreshToken = jwt.sign(payload, process.env.JWT_REFRESH_SECRET, { expiresIn: '30d' })
+    return {
+      accessToken,
+      refreshToken
+    }
+  }
+
+  async saveToken(userId, refreshToken) {
+    const tokenData = tokenModel.findOne({ user: userId })
+
+    if (tokenData) {
+      tokenData.refreshToken = refreshToken
+      return tokenData.save()
+    }
+
+    const token = tokenModel.create({ user: userId, refreshToken })
+    return token
+  }
+
+  // TODO: Продумать логику для ситуации когда юзер может заходить периодически с нескольких устройств. Что бы токены в БД не затирались
+}
 
 export default new TockenService()

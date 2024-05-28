60c09987c4385f8ce29670afce739be430412c4d
nik_nik <nikbox88@gmail.com>
2024-05-28 23:36:25 +0300

    + methods of class AppiError

--- server/exceptions/api-error.js
+++ server/exceptions/api-error.js
@@ -1,11 +1,18 @@
 export default class AppiError extends Error {
-  status;
-  errors;
+  status
+  errors
 
-  constructor(status, message, errors=[]){
-    super(message);
-    this.status;
-    this.errors;
+  constructor(status, message, errors = []) {
+    super(message)
+    this.status = status
+    this.errors = errors
+  }
+
+  static UnauthorizedErrors() {
+    return new AppiError(401, 'Пользователь не авторизован')
+  }
+
+  static BadRequest(message, errors) {
+    return new AppiError(400, message, errors)
   }
 }
-
